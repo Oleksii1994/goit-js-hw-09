af8f0368a8d6cabe8423919bc0c3a583c198dcf3
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-02-19 17:47:46 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  bodyEl: document.querySelector('body'),
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
 
+let timerId = null;
+
+function getRandomHexColor() {
+  return `${`#${Math.floor(Math.random() * 16777215).toString(16)}`}`;
+}
+
+function onBtnStartClick() {
+  timerId = setInterval(() => {
+    refs.bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+    refs.btnStart.setAttribute('disabled', 'true');
+  }, 1000);
+}
+
+function onBtnStopClick() {
+  clearInterval(timerId);
+  refs.btnStart.removeAttribute('disabled');
+}
+
+refs.btnStart.addEventListener('click', onBtnStartClick);
+refs.btnStop.addEventListener('click', onBtnStopClick);
