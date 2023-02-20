aed47695bddfa05b83cd7b44b7fae639dd5192c3
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-02-20 23:05:32 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -23,8 +23,8 @@ formEl.addEventListener('submit', event => {
     });
   }
 
-  for (let i = 0; i < amountValue; i++) {
-    const position = i + 1;
+  for (let i = 0; i < amountValue; i += 1) {
+    const position = i;
     const promiseDelay = delayValue + i * stepValue;
 
     createPromise(position, promiseDelay)
@@ -36,64 +36,3 @@ formEl.addEventListener('submit', event => {
       });
   }
 });
-
-// createPromise(refs.amountInput.value, refs.delayInput.value).then(
-//   ({ position, delay }) => {
-//     // counterPosition();
-//     // position = 0;
-//     position += 1;
-//     delay = refs.delayInput.value;
-//     delay += refs.stepInput.value;
-
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   }
-// );
-//   .catch(({ position, delay }) => {
-//     // position = 0;
-//     // position += 1;
-//     // delay = refs.delayInput.value;
-//     // delay += refs.stepInput.value;
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-
-// function counterPosition() {
-//   let position = 0;
-//   return (position += 1);
-// }
-
-// const form = document.querySelector('.form');
-// form.addEventListener('submit', event => {
-//   event.preventDefault(); // перешкоджає формі відправити дані на сервер
-
-//   // отримуємо значення з полів форми
-//   const delay = Number(form.delay.value);
-//   const step = Number(form.step.value);
-//   const amount = Number(form.amount.value);
-
-//   // функція для створення промісу з вказаною затримкою
-//   function createPromise(position, delay) {
-//     return new Promise((resolve, reject) => {
-//       const shouldResolve = Math.random() > 0.3;
-//       setTimeout(() => {
-//         if (shouldResolve) {
-//           resolve({ position, delay });
-//         } else {
-//           reject({ position, delay });
-//         }
-//       }, delay);
-//     });
-//   }
-
-//   // створюємо вказану кількість промісів
-//   for (let i = 0; i < amount; i++) {
-//     const position = i + 1;
-//     const promiseDelay = delay + i * step; // збільшуємо затримку для кожного наступного промісу
-//     createPromise(position, promiseDelay)
-//       .then(({ position, delay }) => {
-//         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//       })
-//       .catch(({ position, delay }) => {
-//         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//       });
-//   }
-// });
